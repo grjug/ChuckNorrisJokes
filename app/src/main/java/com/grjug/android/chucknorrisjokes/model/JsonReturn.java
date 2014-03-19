8277d43dc5c4b16c166df84a3ef6f1f415887b21
Carlus Henry <carlushenry@gmail.com>
2014-03-19 07:11:34 -0400

    adding getters and setters for model objects.

--- app/src/main/java/com/grjug/android/chucknorrisjokes/model/JsonReturn.java
+++ app/src/main/java/com/grjug/android/chucknorrisjokes/model/JsonReturn.java
@@ -5,8 +5,32 @@ import java.util.List;
 /**
  * Created by emonk on 3/18/14.
  */
-public class jsonReturn {
+public class JsonReturn {
     List<Joke> jokeList;
     List<String> categories;
-    int count;
+    Integer count;
+
+    public List<Joke> getJokeList() {
+        return jokeList;
+    }
+
+    public void setJokeList(List<Joke> jokeList) {
+        this.jokeList = jokeList;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
 }
