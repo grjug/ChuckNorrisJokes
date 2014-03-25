a7a9b00af69f238c9f6b1007fc49a44fc3eafa0d
Eric Fox <foxefj@gmail.com>
2014-03-24 20:52:10 -0500

    updating to include basic buttons for the other parts of the app

--- app/src/main/java/com/grjug/android/chucknorrisjokes/ui/MainActivity.java
+++ app/src/main/java/com/grjug/android/chucknorrisjokes/ui/MainActivity.java
@@ -1,13 +1,10 @@
 package com.grjug.android.chucknorrisjokes.ui;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
