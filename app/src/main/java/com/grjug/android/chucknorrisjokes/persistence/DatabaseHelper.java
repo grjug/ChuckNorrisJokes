7186a0bfb97951ebe84a762cb4fcaef87ae9664d
Joe Langford <joseph.langford@gmail.com>
2014-04-15 17:17:35 -0400

    updating dao and crud

--- app/src/main/java/com/grjug/android/chucknorrisjokes/persistence/DatabaseHelper.java
+++ app/src/main/java/com/grjug/android/chucknorrisjokes/persistence/DatabaseHelper.java
@@ -1,8 +1,14 @@
 package com.grjug.android.chucknorrisjokes.persistence;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.grjug.android.chucknorrisjokes.model.Joke;
+
+import java.util.Date;
+
 /**
  * Created by emonk on 4/14/14.
  */
@@ -25,7 +31,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String KEY_CREATED_AT = "created_at";
 
     // Joke Table - column names
-    private static final String KEY_JOKE = "joke";
     private static final String KEY_JOKE_TEXT = "joke_text";
     private static final String KEY_THUMBS_UP = "thumbs_up";
 
@@ -39,20 +44,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     // Table Create Statements
     // Joke table create statement
-    private static final String CREATE_TABLE_TODO = "CREATE TABLE "
+    private static final String CREATE_TABLE_JOKE = "CREATE TABLE "
             + TABLE_JOKE + "(" + KEY_ID + " INTEGER PRIMARY KEY," + KEY_JOKE_TEXT
             + " TEXT," + KEY_THUMBS_UP + " INTEGER," + KEY_CREATED_AT
             + " DATETIME" + ")";
 
-    // Tag table create statement
-    private static final String CREATE_TABLE_TAG = "CREATE TABLE " + TABLE_TAG
-            + "(" + KEY_ID + " INTEGER PRIMARY KEY," + KEY_TAG_NAME + " TEXT,"
+    // Category table create statement
+    private static final String CREATE_TABLE_CATEGORY = "CREATE TABLE " + TABLE_CATEGORY
+            + "(" + KEY_ID + " INTEGER PRIMARY KEY," + KEY_CATEGORY_NAME + " TEXT,"
             + KEY_CREATED_AT + " DATETIME" + ")";
 
-    // todo_tag table create statement
-    private static final String CREATE_TABLE_TODO_TAG = "CREATE TABLE "
-            + TABLE_TODO_TAG + "(" + KEY_ID + " INTEGER PRIMARY KEY,"
-            + KEY_TODO_ID + " INTEGER," + KEY_TAG_ID + " INTEGER,"
+    // joke_to_category table create statement
+    private static final String CREATE_TABLE_JOKE_TO_CATEGORY = "CREATE TABLE "
+            + TABLE_JOKE_TO_CATEGORY + "(" + KEY_ID + " INTEGER PRIMARY KEY,"
+            + KEY_JOKE_ID + " INTEGER," + KEY_CATEGORY_ID + " INTEGER,"
             + KEY_CREATED_AT + " DATETIME" + ")";
 
     public DatabaseHelper(Context context) {
@@ -64,12 +69,46 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_JOKE);
+        db.execSQL(CREATE_TABLE_CATEGORY);
+        db.execSQL(CREATE_TABLE_JOKE_TO_CATEGORY);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // on upgrade drop older tables
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_JOKE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CATEGORY);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_JOKE_TO_CATEGORY);
 
+        // create new tables
+        onCreate(db);
+    }
+
+    /*
+ * Creating a joke
+ */
+    public long createJoke(Joke joke, Integer thumbsUp,  long[] cat_ids) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_JOKE_TEXT, joke.getJokeText());
+        values.put(KEY_THUMBS_UP, thumbsUp);
+        values.put(KEY_CREATED_AT, new Date().toString());
+
+        // insert row
+        long joke_id = db.insert(TABLE_JOKE, null, values);
+
+        // assigning categories to joke
+        for (long cat_id : cat_ids) {
+            createJokeToCategory(joke_id, cat_id);
+        }
+
+        return joke_id;
+    }
+
+    private long createJokeToCategory(long joke_id, long cat_id) {
+        return 0;
     }
 }
