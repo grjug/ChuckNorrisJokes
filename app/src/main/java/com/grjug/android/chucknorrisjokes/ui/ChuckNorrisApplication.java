f2c040b180e2af44b369a9c0504550141b52b5ee
Carlus Henry <carlushenry@gmail.com>
2014-08-31 21:27:45 -0400

    adding dagger dependencies

--- app/src/main/java/com/grjug/android/chucknorrisjokes/ui/ChuckNorrisApplication.java
+++ app/src/main/java/com/grjug/android/chucknorrisjokes/ui/ChuckNorrisApplication.java
@@ -1,18 +1,37 @@
 package com.grjug.android.chucknorrisjokes.ui;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.grjug.android.chucknorrisjokes.persistence.DatabaseHelper;
+
+import dagger.ObjectGraph;
+import dagger.internal.Modules;
 
 /**
  * Created by carlushenry on 5/11/14.
  */
 public class ChuckNorrisApplication extends Application {
+    private ObjectGraph objectGraph;
+
     @Override
     public void onCreate() {
         super.onCreate();
 
         init();
+    }
+
+    public void buildObjectGraphAndInject() {
+        objectGraph = ObjectGraph.create(Modules.list(this));
+        objectGraph.inject(this);
+    }
+
+    public void inject(Object o) {
+        objectGraph.inject(o);
+    }
+
+    public static ChuckNorrisApplication get(Context context) {
+        return (ChuckNorrisApplication) context.getApplicationContext();
     }
 
     private void init() {
